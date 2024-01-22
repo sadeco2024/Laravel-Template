c4567d3e04c638ea17f0768becd68a6461ce9f5c
sadeco2024 <150572673+sadeco2024@users.noreply.github.com>
2024-01-21 23:02:14 -0600

    resources

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,26 @@ export default defineConfig({
                 'resources/js/auth-app.js',
                 'resources/js/alerts.js',
                 'resources/assets/images/brand-logos/logo-70.png',
-                'resources/assets/js/coming-soon.js'
+                'resources/assets/js/coming-soon.js',
+                'resources/assets/images/brand-logos/toggle-logo.png',
+                'resources/assets/libs/bootstrap/css/bootstrap.min.css',
+                'resources/assets/css/styles.css',
+                'resources/assets/libs/choices.js/public/assets/scripts/choices.min.js',
+                'resources/assets/js/main.js',
+                'resources/assets/images/media/loader.svg',
+                'resources/assets/images/faces/8.jpg',
+                'resources/assets/libs/@popperjs/core/umd/popper.min.js',
+                'resources/assets/libs/bootstrap/js/bootstrap.bundle.min.js',
+                'resources/assets/js/defaultmenu.min.js',
+                'resources/assets/libs/node-waves/waves.min.js',
+                'resources/assets/js/sticky.js',
+                'resources/assets/libs/simplebar/simplebar.min.js',
+                'resources/assets/js/simplebar.js',
+                'resources/assets/libs/@simonwep/pickr/pickr.es5.min.js',
+                'resources/assets/js/custom-switcher.min.js',
+                'resources/assets/js/custom.js',
+                
+
             ],
 
             
